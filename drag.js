11eb5b38e987f2c3d3dac6f92bb2a1d5d7954267
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2021-09-14 22:08:59 +0300

    drag exercises

---
+++ drag.js
@@ -0,0 +1,60 @@
+// let thumb=document.getElementById("thumb");
+// let slider=document.getElementById("slider");
+
+// thumb.onmousedown=function(e){
+//     e.preventDefault();
+//     let shiftX = e.clientX - thumb.getBoundingClientRect().left;
+//     document.addEventListener("mousemove",moveslider);
+//     document.addEventListener("mouseup",onMouseUp);
+ 
+
+//     function moveslider(e)
+//         {
+//            let newLeft=  e.clientX - shiftX - slider.getBoundingClientRect().left;
+//             if (newLeft < 0)
+//              {
+//                 newLeft = 0;
+//              }
+//             let rightEdge = slider.offsetWidth - thumb.offsetWidth;
+//             if (newLeft > rightEdge)
+//              {
+//                 newLeft = rightEdge;
+//             }
+  
+//              thumb.style.left = newLeft + 'px';
+//         }
+    
+
+//         function onMouseUp() {
+//             document.removeEventListener('mouseup', onMouseUp);
+//             document.removeEventListener('mousemove', moveslider);
+//           }
+// }
+// thumb.ondragstart=function(){return false};
+
+const field=document.getElementById("field");
+field.onmousedown=function(e){
+    e.preventDefault();
+    let shiftY = e.clientY - field.getBoundingClientRect().top;
+    document.addEventListener("mousemove",moveheroes);
+    document.addEventListener("mouseup",onMouseTop);
+
+    function moveheroes(e)
+    {
+       let newTop=  e.clientY - shiftY - slider.getBoundingClientRect().top;
+        if (newTop < 0)
+         {
+            newTop = 0;
+         }
+
+         thumb.style.top = newTop + 'px';
+    }
+
+
+    function onMouseTop() {
+        document.removeEventListener('mouseup', onMouseTop);
+        document.removeEventListener('mousemove', moveheroes);
+      }
+}
+
+field.ondragstart=function(){return false};
